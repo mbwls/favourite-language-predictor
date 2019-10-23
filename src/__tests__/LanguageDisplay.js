df54d82ea28f40abce885248bfa2e0e0884ad149
Matthew Bowles <matto.bowles@gmail.com>
2019-10-23 19:15:56 +0100

    Configured test framework

--- src/__tests__/LanguageDisplay.js
+++ src/__tests__/LanguageDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ResultOutput from '../components/LanguageDisplay';
+import LanguageDisplay from '../components/LanguageDisplay';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/__tests__/LanguageFinder.js
+++ src/__tests__/LanguageFinder.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NameInput from '../components/LanguageFinder';
+import LanguageFinder from '../components/LanguageFinder';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
