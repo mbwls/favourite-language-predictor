020da5dee065731db891acc59879269156bd4603
Matthew Bowles <matto.bowles@gmail.com>
2019-10-23 18:13:52 +0100

    Updated favicon and app title

--- src/__tests__/LanguageDisplay.js
+++ src/__tests__/LanguageDisplay.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NameInput from '../components/NameInput';
+import ResultOutput from '../components/LanguageDisplay';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NameInput />, div);
+  ReactDOM.render(<LanguageDisplay />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import '../styles/Header.css';
 const Header = () => {
     return (
         <div>
-            <h1 className='detective-header'>🕵</h1>
+            <h1 className='detective-header'><span role='img' aria-label='detective'>🕵</span></h1>
             <h1 className='title-header'>LANGUAGE<br/>SLEUTH</h1>
-            <h4>Enter a GitHub username below to determine the user's favourite programming&nbsp;language.</h4>
+            <h4>Enter a GitHub username to determine the user's favourite programming&nbsp;language.</h4>
         </div>
     )
 }
