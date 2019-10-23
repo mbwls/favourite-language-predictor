75535bfd926590c36c4971e92f03623acc63e09f
Matthew Bowles <matto.bowles@gmail.com>
2019-10-23 11:22:48 +0100

    Added button for searching

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,47 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const NameInput = props => {
     // BEGIN HOOKS
     const [username, setUsername] = useState('');
     const [searchKey, setSearchKey] = useState(username);
+    const [favLang, setFavLang] = useState('');
 
     useEffect(() => {
-        if (searchKey !== '') {
+        if (searchKey !== '')
             fetch(`https://api.github.com/users/${searchKey}/repos`)
                 .then((resp) => {
-                    if (resp.status !== 200) return;
+                    if (resp.status !== 200) {
+                        console.log(resp);
+                        return;
+                    };
 
                     resp.json().then((data) => {
-                        let favLanguage = _.head(_(_.map(data, 'language'))
+                        console.log('errrr', data.message)
+                        setFavLang(_.head(_(_.map(data, 'language'))
                             .countBy()
                             .entries()
-                            .maxBy(_.last));
-                        console.log('your fav language', favLanguage);
+                            .maxBy(_.last)));
                     })
                 })
                 .catch((err) => {
                     console.log('FETCH ERROR', err)
                 })
-        }
     }, [searchKey]);
     // END HOOKS
 
+    // BEGIN EVENT HANDLERS
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') setSearchKey(event.target.value);
     }
 
+    const handleClick = (event) => {
+        setSearchKey(username);
+    }
+    // END EVENT HANDLERS
+
     return (
-        <TextField
-            id='username'
-            label='Github Username'
-            margin='normal'
-            variant='outlined'
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-            onKeyPress={handleKeyPress}
-        />
+        <React.Fragment>
+            <TextField
+                id='username'
+                label='Github Username'
+                margin='normal'
+                variant='outlined'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                onKeyPress={handleKeyPress}
+            />
+            <Button
+                style={{'backgroundColor':'#e0e0e0'}}
+                onClick={handleClick}>
+                    SEARCH
+            </Button>
+            <h3>{`Your favourite language is ${favLang}`}</h3>
+        </React.Fragment>
     )
 }
 
